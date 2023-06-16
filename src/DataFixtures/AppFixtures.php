88474df54a6faea9e7f7b1434196cd49ca5d510d
Yvelin <gerbeaudy@yahoo.fr>
2023-06-16 19:09:23 +0200

    User & userListener

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,9 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Vehicle;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -16,10 +18,20 @@ class AppFixtures extends Fixture
         for ($i = 0; $i <= 50; $i++){
             $vehicle = new Vehicle;
             $vehicle->setName('Vehicle ' .  $i)
-                ->setPrice(mt_rand(100, 1000));
+                ->setPrice(mt_rand(100, 1000))
+                -> setImage('public\images\uploads\vehicle\Look-at-me-648713d86c46d.png');
+                
 
             $manager->persist($vehicle);
         }
+
+        $user = new User();
+        
+        $user->setUsername('user')
+        ->setRoles(['ROLE_USER'])
+        ->setPlainpassword('password');
+
+        $manager->persist($user);
 
         $manager->flush();
     }
